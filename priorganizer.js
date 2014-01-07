163fd86e75d533f2550826fcea4177dc9382d5d8
Eric Jorgenson <jorgenric@gmail.com>
2014-01-06 22:32:19 -0800

    Initial

---
+++ priorganizer.js
@@ -0,0 +1,5 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.0/jquery.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/knockout/2.3.0/knockout-min.js"></script>
+
+
+
